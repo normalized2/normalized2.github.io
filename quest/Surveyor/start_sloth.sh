c2ce2a26e9eed7cf90b934fff54826b1508b8b2f
normalized2 <normalized2@hotmail.com>
2019-01-10 18:20:17 +0300

    7103-7104

---
+++ quest/Surveyor/start_sloth.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# sloth is a tool for labeling image and video data for computer vision research.
+# https://github.com/cvhciKIT/sloth
+
+~/a/sloth/sloth/bin/sloth  --config=sloth_config.py a.json
